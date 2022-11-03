479ad98feb40ce3e0de21157094ea8dea1e029a4
Rik Janssen <56626554+rik-janssen@users.noreply.github.com>
2022-11-03 08:39:25 +0100

    Update Shorthand.php

--- src/Shorthand.php
+++ src/Shorthand.php
@@ -4,5 +4,37 @@ namespace Config;
 
 class Shorthand{
 
+  public static function get($array, $path){
+
+    $path = explode('/', $path);
+      
+      foreach($path as $bit) {
+
+          if(isset($array[$bit])) {
+
+            /**
+             * Attach a new
+             * config bit.
+             */
+
+            $array = $array[$bit];
+
+          }else{
+
+            /**
+             * When the value is 
+             * not set in the config
+             * file. Return false.
+             */
+
+            return false;
+            
+          }
+
+      }
+    
+      return $array;
+    
+  }
 
 }
