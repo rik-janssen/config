55bf75c4d7c4c138fab8ac9fd696d9dd82b89c7b
Rik Janssen <56626554+rik-janssen@users.noreply.github.com>
2022-11-03 08:31:43 +0100

    Create config.php

---
+++ test/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// This is an example config file.
+// 1. copy this file to a suitable place in your project where it won't be overwritten.
+// 2. include this file.
+// 3. use Config::get('site/url') to pull information from the config file.
+// 4. the config file can be adjusted to your projects needs by adding keys/values/arrays.
+
+$config = [
+];
+
+
+define("CONFIG", $config);
